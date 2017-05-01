f6f87b9bcf9390a86de34f78b1c3f9afafd017e2
Jan Phillip Carreon <janphillipcarreon@gmail.com>
2017-05-01 04:29:26 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash={}
+  data.each do |info, info_hash|
+    info_hash.each do |key, array|
+      array.each do |value|
+        if !new_hash.has_key?(value)
+          new_hash[value]={}
+        end
+        if !new_hash[value].has_key?(info)
+          new_hash[value][info]=[]
+        end
+
+        if array.include?(value)
+          new_hash[value][info] << key.to_s
+        end
+      end
+    end
+  end
+   new_hash
 end
